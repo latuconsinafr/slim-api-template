1cf913be33ca5254bb9e306715db5601d4f81b81
Farista Latuconsina <faristalatuconsina@gmail.com>
2021-11-03 14:40:02 +0700

    feat: Basic controller

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,3 +1,32 @@
 <?php
 
-namespace App\Controllers\Home;
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class HomeController
+{
+
+    /**
+     * The constructor
+     */
+    public function __construct()
+    {
+    }
+
+
+    /**
+     * The index
+     * 
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * 
+     * @return ResponseInterface
+     */
+    public function index(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        return $response;
+    }
+}
