9e55005c96657bc688ac831634e22e2e3e3f5668
Farista Latuconsina <faristalatuconsina@gmail.com>
2021-11-18 13:13:25 +0700

    feat: Add phpdotenv support

--- config/settings.php
+++ config/settings.php
@@ -3,52 +3,60 @@
 declare(strict_types=1);
 
 // Configure defaults for the whole application.
+$root = dirname(__DIR__);
 
 // Error reporting
 error_reporting(0);
 ini_set('display_errors', '0');
 ini_set('display_startup_errors', '0');
 
+// Load configuration based on environment
+$dotenv = (Dotenv\Dotenv::createMutable($root))->safeLoad();
+$env = '.env' . '.' . strtolower($_ENV['APP_ENVIRONMENT'] ?? '');
+if (file_exists($root . '/' . $env)) {
+    $dotenv = array_merge($dotenv, (Dotenv\Dotenv::createMutable($root, $env))->safeLoad());
+}
+
 // Timezone
-date_default_timezone_set('Asia/Jakarta');
+date_default_timezone_set($_ENV['APP_TIMEZONE']);
 
 // Initialize settings
-$settings = [];
+$settings = [
 
-// Path settings
-$settings['root'] = dirname(__DIR__);
-$settings['public'] = $settings['root'] . '/public';
-$settings['template'] = $settings['root'] . '/templates';
-$settings['entity'] = $settings['root'] . '/src/Data/Entities';
+    // Path settings
+    'public'    => $root . '/public',
+    'template'  => $root . '/templates',
+    'entity'    => $root . '/src/Data/Entities',
 
-// Error handler settings
-$settings['error'] = [
-    'display_error_details' => true,
-    'log_errors' => true,
-    'log_error_details' => true,
-];
-
-// Logger settings
-$settings['logger'] = [
-    'name' => 'app',
-    'path' => $settings['root'] . '/logs',
-    'filename' => 'app.log',
-    'level' => \Monolog\Logger::INFO,
-    'file_permission' => 0775,
-];
-
-// Database settings
-$settings['database'] = [
-    'default'     => 'default',
-    'databases'   => [
-        'default' => ['connection' => 'mysql']
+    // Error handler settings
+    'error' => [
+        'display_error_details' => $_ENV['ERROR_DISPLAY_DETAILS'],
+        'log_errors'            => $_ENV['ERROR_LOG'],
+        'log_error_details'     => $_ENV['ERROR_LOG_DETAILS'],
     ],
-    'connections' => [
-        'mysql' => [
-            'driver'  => Spiral\Database\Driver\MySQL\MySQLDriver::class,
-            'connection' => "mysql:host=localhost;dbname=slim-api-db",
-            'username'   => "root",
-            'password'   => "",
+
+    // Logger settings
+    'logger' => [
+        'name'              => $_ENV['LOGGER_NAME'],
+        'path'              => $root . '/' . $_ENV['LOGGER_PATH'],
+        'filename'          => $_ENV['LOGGER_FILENAME'],
+        'level'             => $_ENV['LOGGER_DEBUG'] ? \Monolog\Logger::DEBUG : \Monolog\Logger::INFO,
+        'file_permission'   => 0775,
+    ],
+
+    // Database settings
+    'database' => [
+        'default'     => 'default',
+        'databases'   => [
+            'default' => ['connection' => $_ENV['DB_CONNECTION']]
+        ],
+        'connections' => [
+            'mysql' => [
+                'driver'        => Spiral\Database\Driver\MySQL\MySQLDriver::class,
+                'connection'    => "mysql:host={$_ENV['DB_HOST']}:{$_ENV['DB_PORT']};dbname={$_ENV['DB_DBNAME']}",
+                'username'      => $_ENV['DB_USERNAME'],
+                'password'      => $_ENV['DB_PASSWORD'],
+            ]
         ]
     ]
 ];
